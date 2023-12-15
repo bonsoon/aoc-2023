d250f7992f2bcc7050a047dd0cc6bcf49c65051b
bonsoon <bonsoon@gmail.com>
2023-12-15 15:01:47 -0800

    format

--- day15/day15.js
+++ day15/day15.js
@@ -14,16 +14,24 @@
 // https://adventofcode.com/2023/day/15
 // https://adventofcode.com/2023/day/15/input
 
-stream = document.body.innerText
-data = stream.replace('\r|\n', '').split(',')
-
-A  = w => w.split('').reduce((a,v)=>((a+v.charCodeAt())*17)%256,0)
-P1 = m => m.reduce((a,v)=>a+A(v),0)
-B = m => (b=new Map(),m.map(x=>x.split(/-|=/)).forEach(e=>(f=e[1])
-    ==''?(b.get(A(l=e[0]))?.delete(l)):b.has(n=A(l=e[0]))?b.get
-    (n).set(l,f):b.set(n,new Map([e]))),b)
-P2 = m => [...B(m).entries()].reduce((a,b)=>a+[...b[1].entries()]
-    .reduce((c,l,p)=>c+(b[0]+1)*(p+1)*(0+l[1]),0),0)
-
-console.log('Part 1 is ... ', P1(data))
-console.log('Part 2 is ... ', P2(data))
+                      stream = 
+             document         .body
+         .innerText                 ;
+     data =stream                    .
+   replace('\r|\n'
+  ,'').split ( ','                   );
+  A = w=>w.split(                    '').
+ reduce(( a,v ) =>                  ((a+v.
+ charCodeAt())*17)%               256,0);P1
+ =m => m.reduce((a,v)           =>a+A(v),0);
+ B = m => (b = new Map () ,m.map(x=>x. split
+ (/-|=/)).forEach(e =>(f=e[1])==''?(b.get(A
+ (l=e[0]))?.delete(l)):b.has(n=A(l=e[0]))?b
+ .get(n).set(l,f):b.set(n,new Map([e]))),b)
+  ;P2 = m => [...B(m).entries()].reduce((a
+   ,b)=>a+[...b[1].entries()].reduce((c,l
+     ,p)=>c+(b[0]+1)*(p+1)*(0+l[1]),0 ),
+      0);console.log('Part 1 is ... ' ,
+        P1(data),'\nPart 2 is ... ',
+           P2(data)); //AOC 2023
+               // DAY 15, by B
