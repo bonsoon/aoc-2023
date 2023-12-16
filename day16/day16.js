a4983add4fdadbd9f3a460e73d1d08c8090a7dfb
bonsoon <bonsoon@gmail.com>
2023-12-16 10:17:00 -0800

    chrome text grab

--- day16/day16.js
+++ day16/day16.js
@@ -1,8 +1,12 @@
 // Day 16
 // Laser bean starts top left going right
+// fs = require('fs')
+// print = console.log
+// stream = fs.readFileSync('input', 'utf8')
+
 stream = document.body.innerText
 
-G = s =>(g=s.replaceAll('\r', '').split('\n'),g.w =g[0].length,g.h=g.length,g)
+G = s =>(g=s.replaceAll(/\r|\n$/g, '').split('\n'),g.w =g[0].length,g.h=g.length,g)
 A = (p,q) => p.map((t,i)=>t+q[i])
 D = n => [[-1,0],[1,0],[0,-1],[0,1]][n]
 N = (c,d) =>c == '/'? [[3],[2],[1],[0]][d]:
@@ -15,29 +19,16 @@ B = (p,d,g,m=new Map())=>{
     else( m.has(p+'')?  m.get(p+'').push(d):m.set(p+'',[d]))
     let [nr,nc] = A(p,D(d))
     if (nr < 0 || nr >= g.h || nc < 0 || nc >= g.w){return}
-    for(let t of N(g[nr][nc],d)){B([nr,nc],t,g,m)}
+    for(let z of N(g[nr][nc],d)){B([nr,nc],z,g,m)}
     return m.size - 1}
 
-P1= (s) => console.log('part 1 ... ', B([0,-1],3,G(s)))
-P2 = (s)=>{
-max = 0
-g = G(s)
-for(let r = 0 ; r < g.h; r++){
-    max = Math.max(B([r,-1],3,g), max)
-}
-for(let r = 0 ; r < g.h; r++){
-    max = Math.max(B([r,g.w],2,g), max)
-}
-for(let c = 0 ; c < g.w; c++){
-    max = Math.max(B([-1,c],1,g), max)
-}
-for(let c = 0 ; c < g.w; c++){
-    max = Math.max(B([g.h,c],0,g), max)
-}
-console.log('part 2 ... ', max)
-}
+P1 = (g) => console.log('part 1 ... ', B([0,-1],3,g))
+P2 = (g) => {let [x,y,dx,dy,r,max,d,i,q] = [-1,-1,1,0,[3,0,2,1].values(),0]
+        for( d = r.next().value, s = 0; s < 4;q=dx,dx=-dy,dy=q, s++){
+            for ( i=1; i<= g.w;x+=dx,y+=dy, i++) {
+                B([x,y],d,g) > max ? max=B([x,y],d,g):null}}
+        console.log('part 2 ... ', max)}
 
-t = performance.now()
-P1(stream)
-P2(stream)
-console.log((performance.now() -t)/1000,' seconds' )
+t = performance.now();
+(g = G(stream),P1(g),P2(g))
+console.log((performance.now() -t)/1000 + ' seconds' )
