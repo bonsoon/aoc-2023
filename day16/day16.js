4541c40473620bf157d3e04c2cae40d603294bd9
bonsoon <bonsoon@gmail.com>
2023-12-16 11:21:06 -0800

    used optional chaining ?.

--- day16/day16.js
+++ day16/day16.js
@@ -9,7 +9,7 @@ D = n => [[-1,0],[1,0],[0,-1],[0,1]][n]
 N = (c,d) => c == '/'? [[3],[2],[1],[0]][d]: c == '\\'? [[2],[3],[0],[1]][d]:
             c == '|'? [[d],[d],[0,1],[0,1]][d]: c == '-'? [[2,3],[2,3],[d],[d]][d]:[d]
 B = (p,d,g,m=new Map())=>{
-    if (m.has(p+'') && m.get(p+'').includes(d)) { return }
+    if (m.get(p+'')?.includes(d)) { return }
     else( m.has(p+'')? m.get(p+'').push(d):m.set(p+'',[d]))
     let ed = D(d), [nr,nc] = [p[0]+ed[0],p[1]+ed[1]]
     if (nr < 0 || nr >= g.h || nc < 0 || nc >= g.w){ return }
