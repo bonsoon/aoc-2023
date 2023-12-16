b0e88b522e21e961757880852763ddf7b09c7e4d
bonsoon <bonsoon@gmail.com>
2023-12-16 11:27:28 -0800

    returns null if not the initial main branch

--- day16/day16.js
+++ day16/day16.js
@@ -8,13 +8,13 @@ G = s => (g=s.replaceAll(/\r|\n$/g, '').split('\n'), g.w=g[0].length, g.h=g.leng
 D = n => [[-1,0],[1,0],[0,-1],[0,1]][n]
 N = (c,d) => c == '/'? [[3],[2],[1],[0]][d]: c == '\\'? [[2],[3],[0],[1]][d]:
             c == '|'? [[d],[d],[0,1],[0,1]][d]: c == '-'? [[2,3],[2,3],[d],[d]][d]:[d]
-B = (p,d,g,m=new Map())=>{
+B = (p,d,g,m=new Map(),ini=1)=>{
     if (m.get(p+'')?.includes(d)) { return }
     else( m.has(p+'')? m.get(p+'').push(d):m.set(p+'',[d]))
     let ed = D(d), [nr,nc] = [p[0]+ed[0],p[1]+ed[1]]
     if (nr < 0 || nr >= g.h || nc < 0 || nc >= g.w){ return }
-    for(let z of N(g[nr][nc],d)){B([nr,nc],z,g,m)}
-    return m.size - 1}
+    for(let z of N(g[nr][nc],d)){B([nr,nc],z,g,m,0)}
+    return ini? (m.size-1):null }
 P1 = (g) => console.log('part 1 ... ', B([0,-1],3,g))
 P2 = (g) => {
     let [x,y,dx,dy,r,max,d,i] = [-1,-1,1,0,[3,0,2,1].values(),0]
