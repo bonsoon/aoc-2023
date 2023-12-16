29fb62fdd29b6914459b8fa2b8db6c6353b5c761
bonsoon <bonsoon@gmail.com>
2023-12-16 10:43:04 -0800

    A was only used "once", removed

--- day16/day16.js
+++ day16/day16.js
@@ -6,21 +6,18 @@ stream = document.body.innerText
 
 G = s => (g=s.replaceAll(/\r|\n$/g, '').split('\n')
                 , g.w=g[0].length, g.h=g.length, g)
-A = (p,q) => p.map((t,i)=>t+q[i])
 D = n => [[-1,0],[1,0],[0,-1],[0,1]][n]
 N = (c,d) => c == '/'? [[3],[2],[1],[0]][d]:
             c == '\\'? [[2],[3],[0],[1]][d]:
             c == '|'? [[d],[d],[0,1],[0,1]][d]:
             c == '-'? [[2,3],[2,3],[d],[d]][d]:[d]
-
 B = (p,d,g,m=new Map())=>{
     if (m.has(p+'') && m.get(p+'').includes(d) ) { return }
     else( m.has(p+'')? m.get(p+'').push(d):m.set(p+'',[d]))
-    let [nr,nc] = A(p,D(d))
+    let [nr,nc] = p.map((t,i)=>t+D(d)[i])
     if (nr < 0 || nr >= g.h || nc < 0 || nc >= g.w){ return }
     for(let z of N(g[nr][nc],d)){B([nr,nc],z,g,m)}
     return m.size - 1}
-
 P1 = (g) => console.log('part 1 ... ', B([0,-1],3,g))
 P2 = (g) => {
     let [x,y,dx,dy,r,max,d,i,_] = [-1,-1,1,0,[3,0,2,1].values(),0]
@@ -34,7 +31,6 @@ t = performance.now();
 console.log((performance.now() -t)/1000 + ' seconds' )
 
 // G = parses the input data, and appends grid width and height ; 
-// A = a simple vector adding function ; 
 // D = encodes directions with 0,1,2,3 ; 
 // N = the workhorse that finds the new direction, given future grid
 //     type and current direction ; 
