b24d79b6888bf0c3ad710761b00a533930e9e951
bonsoon <bonsoon@gmail.com>
2023-12-16 11:04:47 -0800

    removed some comma operators

--- day16/day16.js
+++ day16/day16.js
@@ -9,22 +9,21 @@ D = n => [[-1,0],[1,0],[0,-1],[0,1]][n]
 N = (c,d) => c == '/'? [[3],[2],[1],[0]][d]: c == '\\'? [[2],[3],[0],[1]][d]:
              c == '|'? [[d],[d],[0,1],[0,1]][d]: c == '-'? [[2,3],[2,3],[d],[d]][d]:[d]
 B = (p,d,g,m=new Map())=>{
-    if (m.has(p+'') && m.get(p+'').includes(d) ) { return }
+    if (m.has(p+'') && m.get(p+'').includes(d)) { return }
     else( m.has(p+'')? m.get(p+'').push(d):m.set(p+'',[d]))
-    let [nr,nc] = p.map((t,i)=>t+D(d)[i])
+    let [nr,nc] = p.map((t,i) => t+D(d)[i])
     if (nr < 0 || nr >= g.h || nc < 0 || nc >= g.w){ return }
     for(let z of N(g[nr][nc],d)){B([nr,nc],z,g,m)}
     return m.size - 1}
 P1 = (g) => console.log('part 1 ... ', B([0,-1],3,g))
 P2 = (g) => {
     let [x,y,dx,dy,r,max,d,i] = [-1,-1,1,0,[3,0,2,1].values(),0]
-    for( d = r.next().value, s = 0; s < 4; [dy,dx]=[dx,-dy], s++){
-        for ( i=1; i<= g.w; x+=dx, y+=dy, i++) {
-            B([x,y],d,g) > max ? max=B([x,y],d,g):null}}
+    for(d = r.next().value, s = 0; s < 4; [dy,dx]=[dx,-dy], s++){
+        for (i = 1; i <= g.w; x+=dx, y+=dy, i++) {
+            B([x,y],d,g) > max ? max=B([x,y],d,g) : null}}
     console.log('part 2 ... ', max)}
 
-t = performance.now();
-(g = G(stream),P1(g),P2(g))
+t = performance.now(); g = G(stream); P1(g) ; P2(g)
 console.log((performance.now() -t)/1000 + ' seconds' )
 
 // G = parses the input data, and appends grid width and height ; 
