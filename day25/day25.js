913f407783237172fd2f0685e6fe7b0da61b1e26
bonsoon <bonsoon@gmail.com>
2023-12-24 23:28:27 -0800

    merry christmas!

--- day25/day25.js
+++ day25/day25.js
@@ -1,16 +1,32 @@
-    fs = require('fs')
-    print = console.log
-    stream = fs.readFileSync('test.txt', 'utf8')
-    data = stream.replace(/\r/,'')
+const { format } = require('path')
 
-    bound = 26987.89
-    vertices = (data.match(/middle(.*?)font/g))
-    count = 0
-    num_vertices = 0 
-    for(let line of vertices){
-        num_vertices++
-        if((parseFloat(line.split("x=\"")[1].split("\" y=")[0])) <= 26987.89){
-            count++
-        }
+fs = require('fs')
+print = console.log
+stream = fs.readFileSync('test.txt', 'utf8')
+data = stream.replace(/\r/,'')
+
+bound = 26987.89
+vertices = (data.match(/middle(.*?)font/g))
+count = 0
+num_vertices = 0 
+for(let line of vertices){
+    num_vertices++
+    if((parseFloat(line.split("x=\"")[1].split("\" y=")[0])) <= 26987.89){
+        count++
     }
-    print(count * (num_vertices - count))
+}
+print(count * (num_vertices - count))
+
+// Note to self: 
+// Graphviz dot file format
+
+// graph{
+//     a -- {b c d}
+//     b -- {x y}
+// }
+
+// and a -> b for direced edge
+
+// and to run in cmd:
+// dot -Tsvg input.dot > output.Tsvg
+// or use -K sfdp tag for a different rendering
